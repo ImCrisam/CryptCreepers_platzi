1c68027fad9b61f6c0eda2d5e13d4aca926a4eab
imcris <zanardy20@hotmail.com>
2021-01-25 01:48:37 -0500

    add damager for bullet

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
 
-    [SerializeField] float speed = 5f;
+    [SerializeField] float speed = 10f;
+
+    private void Start() {
+        Destroy(gameObject, 3);
+    }
     void Update()
     {
         transform.position += transform.right * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("enemies"))
+        {
+        other.GetComponent<EnemyController>().TakeDamage();
+
+        }
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+   [SerializeField]float health =1;
+
+    public void TakeDamage()
+    {
+        health--;
+    }
+}
