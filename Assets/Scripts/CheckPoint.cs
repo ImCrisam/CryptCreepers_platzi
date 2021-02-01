603ef6ec1a39160e3d3fe330fc7115b0b8b18918
imcris <zanardy20@hotmail.com>
2021-02-01 02:25:32 -0500

    add spawn items

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField]int value = 10;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.instance.time += value;
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    [SerializeField]GameObject checkPoint;
+    [SerializeField] int delay = 5;
+    Vector2 randonPoint;
+    [SerializeField]float radius=5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnItems());
+    }
+
+    IEnumerator SpawnItems()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            randonPoint = Random.insideUnitCircle * radius;
+            Instantiate(checkPoint, randonPoint, Quaternion.identity);
+        }
+
+    }
+
+
+}
